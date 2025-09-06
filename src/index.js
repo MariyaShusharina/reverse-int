3d77f012526568d8d00f4ec0fd4e6e835c671e33
Mariya Shusharina <mariya.shushar@gmail.com>
2025-09-06 14:27:55 +0300

    feat: solve reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  const str = n.toString().split('');
+  const ret = [];
+  for (let i = 0; i < str.length; i += 1) {
+    ret.unshift(str[i]);
+  }
+  return parseInt(ret.join(''), 10);
 };
